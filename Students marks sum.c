fcd9a50adc527dbc96e7d871d7693ceba001a3f0
Khushi Agarwal <122787112+Khushu27@users.noreply.github.com>
2023-11-08 17:47:44 +0530

    Update Students marks sum.c
    You are given an array of integers, marks, denoting the marks scored by students in a class.
    
    The alternating elements marks0, marks2, marks4 and so on denote the marks of boys.
    Similarly, marks1, marks3, marks5 and so on denote the marks of girls.

--- Students marks sum.c
+++ Students marks sum.c
@@ -1,4 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+//Complete the following function.
+
 int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
   int sum = 0,i=0;
     for( i=(gender=='g'?1:0);i<number_of_students;i=i+2)
    {
@@ -8,3 +16,22 @@ int marks_summation(int* marks, int number_of_students, char gender) {
   return sum;
 }
 
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
+}
